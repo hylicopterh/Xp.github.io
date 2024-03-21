c7ef229ac886efa336cf4041320e1ab9b0ad867b
Xp <2781754216@qq.com>
2024-03-21 17:26:21 +0800

    更新个人简历

--- table_header.js
+++ table_header.js
@@ -6,10 +6,10 @@ document.write("width=\"162.6\" height=\"200\"> <\/td>");
 document.write("<td>");
 document.write("<h1>徐朋</h1>");
 document.write("<br>");
-document.write("<b>机械制造及其自动化</b>");
+document.write("<b>哈尔滨工业大学</b>");
 document.write("<br>");
 
-document.write("<a href=\"https:\/\/www.dlmu.edu.cn\">哈尔滨工业大学<\/a></b>");
+document.write("<a href=\"https:\/\/www.dlmu.edu.cn\">机械制造及其自动化专业<\/a></b>");
 
 document.write("<br>");
 document.write("<br>");
@@ -18,8 +18,8 @@ document.write(" 辽宁省 沈阳市 苏家屯区 <br>");
 document.write("E-mail: 2781754216@qq.com <br>");
 document.write("<br>");
 document.write("&nbsp;");
-document.write("<a href=\"https:\/\/github.com\/HDZ12\"><img src=\"Images\/GitHub-Mark-120px-plus.png\" alt=\"GitHub\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
-document.write("<a href=\"https:\/\/www.zhihu.com\/people\/hdz12\/posts\"> <img src=\"Images\/zhihu.png\" alt=\"Zhihu\"  border=\"0\" height=\"25\"><\/a> &nbsp");
+document.write("<a href=\"https:\/\/github.com\/hylicopterh\"><img src=\"Images\/GitHub-Mark-120px-plus.png\" alt=\"GitHub\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
+document.write("<a href=\"https:\/\/www.zhihu.com\/people\/ming-meng-55-13\"> <img src=\"Images\/zhihu.png\" alt=\"Zhihu\"  border=\"0\" height=\"25\"><\/a> &nbsp");
 document.write("<\/td>");
 document.write("<\/tr>");
 document.write("<\/tbody>");
