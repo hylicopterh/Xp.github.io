fef40593a8ef3a0cf5f59b858704d86484aa5b59
Xp <2781754216@qq.com>
2024-03-27 17:56:37 +0800

    更新网页版简历

--- table_header.js
+++ table_header.js
@@ -9,7 +9,7 @@ document.write("<br>");
 document.write("<b>哈尔滨工业大学</b>");
 document.write("<br>");
 
-document.write("<a href=\"https:\/\/www.dlmu.edu.cn\">机械制造及其自动化专业<\/a></b>");
+document.write("<a href=\"https:\/\/sme.hit.edu.cn/\">机械制造及其自动化专业<\/a></b>");
 
 document.write("<br>");
 document.write("<br>");
@@ -20,6 +20,7 @@ document.write("<br>");
 document.write("&nbsp;");
 document.write("<a href=\"https:\/\/github.com\/hylicopterh\"><img src=\"Images\/GitHub-Mark-120px-plus.png\" alt=\"GitHub\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
 document.write("<a href=\"https:\/\/www.zhihu.com\/people\/ming-meng-55-13\"> <img src=\"Images\/zhihu.png\" alt=\"Zhihu\"  border=\"0\" height=\"25\"><\/a> &nbsp");
+document.write("<a href=\"https:\/\/gitee.com\/people\/small--white\"> <img src=\"Images\/gitee.png\" alt=\"Gitee\" width=\"25\" border=\"0\" height=\"25\"><\/a> &nbsp");
 document.write("<\/td>");
 document.write("<\/tr>");
 document.write("<\/tbody>");
